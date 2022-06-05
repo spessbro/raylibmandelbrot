0c707399fa7a06a4d8147e2350f13d011a0a91ae
Reese <Reesegetsemails@protonmail.com>
2022-06-05 13:30:36 -0600

    Updated naming

---
+++ mandelbrot.glsl
@@ -0,0 +1,48 @@
+#version 400
+
+in vec4 gl_FragCoord;
+in vec4 fragColor;
+
+out vec4 finalColor;
+
+uniform vec2 offset_combined;
+uniform vec2 screen_size;
+uniform float zoom;
+
+#define ITERATION_MAX 256
+#define ITERATION_INC 1
+
+void main() {
+
+    vec2 uv = gl_FragCoord.xy / screen_size;
+
+    vec2 C = vec2(
+        (gl_FragCoord.x/screen_size.x)*zoom-offset_combined.x,
+        (gl_FragCoord.y/screen_size.y)*zoom-offset_combined.y
+        );
+
+    vec2 Z    = vec2(0.0,0.0);
+    vec2 Zsqr = vec2(0.0,0.0);
+
+    int iteration = 0;
+    for (iteration = 0;iteration < ITERATION_MAX;iteration+=ITERATION_INC) {
+        Zsqr = vec2 (
+            Z.x*Z.x,
+            Z.y*Z.y
+            );
+        Z = vec2(
+            Zsqr.x - Zsqr.y,
+            (Z.x + Z.x) * Z.y
+            ) + C;
+
+        if ((Zsqr.x + Zsqr.y) > 4.0) break;
+    }
+
+    float angle = 2 * radians(180) * float(iteration) / float(ITERATION_MAX);
+
+    finalColor.r = 1.0 * (2 * sin(15 * angle + 1) + 1);
+    finalColor.g = 1.0 * (2 * sin(10 * angle + 1) + 1);
+    finalColor.b = 1.0 * (2 * sin(5  * angle + 1) + 1);
+    finalColor.a = 1.0;
+
+}
---
+++ raylibMandelbrot.c
@@ -0,0 +1,104 @@
+#include <raylib.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#define SHADER_NF_ERROR_V "ERROR CHECK LOG"
+
+int main() {
+
+    /* make sure window is square! */
+    const int screenWidth    = 1024;
+    const int screenHeight   = 1024;
+
+    //SetTraceLogLevel(LOG_ERROR);
+    InitWindow(screenWidth, screenHeight, "Fractal Viewer");
+    SetTargetFPS(60);
+
+    Shader mandelbrotShader = LoadShader(0, "mandelbrot.glsl");
+
+    int offsetCombinedLoc  = GetShaderLocation(mandelbrotShader, "offset_combined");
+    int screenCombinedLoc  = GetShaderLocation(mandelbrotShader, "screen_size");
+    int zoomLoc             = GetShaderLocation(mandelbrotShader, "zoom");
+
+    bool errorState = false;
+    if ((offsetCombinedLoc == -1) || (screenCombinedLoc == -1) || (zoomLoc == -1)) {
+        TraceLog(LOG_ERROR,"Shader not found or is corrupted");
+        errorState = true;
+    }
+
+    float zoom               = 3.0;
+    float CxOffset          = 2.2;
+    float CyOffset          = 1.5;
+    float offsetModifier    = 0.1;
+
+    bool redraw              = true;
+    bool screenshotRedraw   = false;
+    bool informationDisplay = true;
+    time_t lastInftogTime  = time(NULL);
+
+
+    while (!WindowShouldClose()) {
+
+        if (IsKeyDown(KEY_KP_1))  {offsetModifier += 0.001;}
+        if (IsKeyDown(KEY_KP_2))  {offsetModifier -= 0.001;}
+        if (IsKeyDown(KEY_KP_4))  {offsetModifier += 0.00001;}
+        if (IsKeyDown(KEY_KP_5))  {offsetModifier -= 0.00001;}
+        if (IsKeyDown(KEY_KP_3))  {zoom -= offsetModifier; redraw = true;}
+        if (IsKeyDown(KEY_KP_6))  {zoom += offsetModifier; redraw = true;}
+        if (IsKeyDown(KEY_UP))    {CyOffset -= offsetModifier; redraw = true;}
+        if (IsKeyDown(KEY_DOWN))  {CyOffset += offsetModifier; redraw = true;}
+        if (IsKeyDown(KEY_LEFT))  {CxOffset += offsetModifier; redraw = true;}
+        if (IsKeyDown(KEY_RIGHT)) {CxOffset -= offsetModifier; redraw = true;}
+        if (IsKeyDown(KEY_SPACE)) {screenshotRedraw = true; redraw = true;}
+        if (IsKeyDown(KEY_H))     {
+            if (time(NULL) > lastInftogTime) {
+                informationDisplay = !informationDisplay;
+                lastInftogTime = time(NULL);
+            }
+        }
+
+        BeginDrawing(); {
+
+            BeginShaderMode(mandelbrotShader); {
+                if (redraw) {
+                    float offsetCombined[2] = {CxOffset, CyOffset};
+                    float screenCombined[2] = {(float)screenWidth, (float)screenHeight};
+
+                    SetShaderValue(mandelbrotShader, offsetCombinedLoc, offsetCombined, SHADER_UNIFORM_VEC2);
+                    SetShaderValue(mandelbrotShader, screenCombinedLoc, screenCombined, SHADER_UNIFORM_VEC2);
+                    SetShaderValue(mandelbrotShader, zoomLoc, &zoom, SHADER_UNIFORM_FLOAT);
+                    redraw = false;
+                }
+                DrawRectangle(0,0,screenWidth,screenHeight,ORANGE);
+            } EndShaderMode();
+
+            if(errorState) {
+                DrawText(SHADER_NF_ERROR_V,(screenWidth-MeasureText(SHADER_NF_ERROR_V,20))/2,screenHeight/2,20,RED);
+            }
+            if (screenshotRedraw) {
+                TakeScreenshot("output.png");
+                screenshotRedraw = false;
+            } else {
+                DrawFPS(10,0);
+                DrawText(
+                        TextFormat("Cx %f Cy %f\nzoom %f offset %f",CxOffset,CyOffset,zoom,offsetModifier)
+                        ,10,30,20,GREEN);
+
+                if (informationDisplay) {
+                    DrawText(
+                        "Controls:\n1/2: Large speed adjust\n4/5: Minor speed adjust\n6/3: Zoom\nArrow keys: Move\nh: Toggle help"
+                        ,10,90,20,BLUE);
+                }
+            }
+        } EndDrawing();
+    }
+
+    UnloadShader(mandelbrotShader);
+    CloseWindow();
+    TraceLog(LOG_INFO, "Exiting");
+
+    return 0;
+}
